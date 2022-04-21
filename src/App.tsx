f089f80ed5d963e848202edeaf49b6c6290a4e86
jvaurof <jvaurof@gmail.com>
2022-04-21 15:39:33 -0300

    configuração do prettier e eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { GlobalStyle } from "./styles/global";
+import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>hello world</h1>
       <GlobalStyle />
     </div>
   );
