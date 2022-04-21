79bd4bbb9305a33aa8482d6de0ea642b8629520b
jvaurof <jvaurof@gmail.com>
2022-04-21 01:00:24 -0300

    criação de estilo global

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --background: #EDF2F7;
+  }
+  
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: var(--background)
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
